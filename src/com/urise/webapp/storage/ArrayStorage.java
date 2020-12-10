2c57dc98ca759f73b9b2a6f6f23d8e69b3a7f86c
vasyan <Kiriluk_v_r@mail.ru>
2020-12-10 11:14:51 +0300

    add interface in MainArray

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,10 +7,8 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage{
-    private static final int STORAGE_LIMIT = 10000;
-    private Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size = 0;
+public class ArrayStorage extends AbstractArrayStorage{
+
 
    public void clear() {
       Arrays.fill(storage, 0, size, null);
@@ -60,9 +58,6 @@ public class ArrayStorage implements Storage{
         return Arrays.copyOfRange(storage, 0 , size);
     }
 
-    public int size() {
-        return size;
-    }
 
     private int getIndex(String uuid) {
         for (int i = 0; i < size; i++)
---
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -0,0 +1,63 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage{
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else  if (size == STORAGE_LIMIT ) System.out.println("Storage overflow");
+        else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
+        } else storage[index] = r;
+
+    }
+
+    public Resume get(String uuid) {
+        int i = getIndex(uuid);
+        if (i != -1 ) {
+            return storage[i];
+        }
+        System.out.println("Resume " + uuid + " not exist");
+        return null;
+    }
+
+    public void delete(String uuid) {
+        int i = getIndex(uuid);
+        if (i != -1 ) {
+            storage[i] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else System.out.println("ERROR");
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0 , size);
+    }
+
+
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++)
+            if (uuid.equals(storage[i].getUuid())) return i;
+        return -1;
+    }
+}
